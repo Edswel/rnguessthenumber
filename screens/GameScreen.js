2a8ab4066274a1fbd9d6ef8f8ca5b1c41a223053
Edswel <franckini@yahoo.com>
2022-04-12 10:56:26 +0100

    Make Title a reuseable component

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
+import Title from '../components/Title';
 
 function GameScreen() {
     return (
         <View style={styles.screen}>
-            <Text>GameScreen</Text>
+            <Title>Your Chosen Number</Title>
             <View>
                 <Text>Higher or Lower</Text>
             </View>
