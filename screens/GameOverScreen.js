d9edd7331b43b8db79ff0c97e6c2d456029992c9
Edswel <franckini@yahoo.com>
2022-04-12 14:50:56 +0100

    Implement game over logic

---
+++ screens/GameOverScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+function GameOverScreen() {
+    return (
+        <View>
+            <Text>GameOverScreen</Text>
+            <Text>GameOverScreen</Text>
+            <Text>GameOverScreen</Text>
+            <Text>GameOverScreen</Text>
+            <Text>GameOverScreen</Text>
+            <Text>GameOverScreen</Text>
+            <Text>GameOverScreen</Text>
+            <Text>GameOverScreen</Text>
+        </View>
+    )
+}
+
+export default GameOverScreen;
